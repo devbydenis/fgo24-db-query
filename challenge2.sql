ad3bbb28989662bf1b5f4b713a7a7b0e5f7a2c59
devbydenis <hello.denisrahmadi@gmail.com>
2025-06-17 11:34:27 +0700

    feat: join actor, roles, movies

--- challenge2.sql
+++ challenge2.sql
@@ -2,8 +2,16 @@ SELECT
   m.name AS movie_title, 
   d.first_name AS director_first_name, 
   mg.genre AS movie_genre 
-FROM movies m
-  JOIN movies_directors md ON m.id = md.movie_id
-  JOIN directors d ON md.director_id = d.id
-  JOIN movies_genres mg ON m.id = mg.movie_id
+  FROM movies m
+    JOIN movies_directors md ON m.id = md.movie_id
+    JOIN directors d ON md.director_id = d.id
+    JOIN movies_genres mg ON m.id = mg.movie_id
 LIMIT 50;
+
+SELECT 
+  a.first_name AS actor_first_name,
+  m.name AS movie_title
+  
+  FROM actors a
+  JOIN roles r ON a.id = r.actor_id
+  JOIN movies m ON r.movie_id = m.id;
